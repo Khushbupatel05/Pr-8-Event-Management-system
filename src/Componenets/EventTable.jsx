c73769798948ccd03d1f0366ffca131225f15cf3
khushbupatel05 <khushbupatel0009@gmail.com>
2025-08-31 10:44:50 +0530

    commit for samll change

--- src/Componenets/EventTable.jsx
+++ src/Componenets/EventTable.jsx
@@ -13,7 +13,7 @@ const EventTable = ({ data }) => {
 
   return (
     <>
-      <section className="">
+      <section >
         <div className="container relative overflow-x-auto shadow-md sm:rounded-lg ">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 
